694397901d24d05f1631c378bbc4be28dd8c5c7c
Gabriel <gabrielmd908@gmail.com>
2022-09-09 15:44:38 -0300

    create chip widget

--- lib/features/pokedex/presentation/pages/home_page.dart
+++ lib/features/pokedex/presentation/pages/home_page.dart
@@ -4,6 +4,9 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_svg/parser.dart';
 import 'package:pokedex_flutter/consts/consts_app.dart';
 import 'package:pokedex_flutter/features/pokedex/presentation/bloc/pokedex/pokedex_bloc.dart';
+import 'package:pokedex_flutter/features/pokedex/presentation/bloc/pokedex_categories/pokedex_categories_bloc.dart';
+import 'package:pokedex_flutter/features/pokedex/presentation/bloc/pokedex_categories/pokedex_categories_state.dart';
+import 'package:pokedex_flutter/features/pokedex/presentation/widgets/categories.dart';
 import '../../../../injection_container.dart' as di;
 
 class HomePage extends StatefulWidget {
@@ -16,7 +19,6 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   void initState() {
-    // di.sl<PokedexBloc>()..add(PokedexTypesFetchList());
     super.initState();
   }
 
@@ -24,54 +26,69 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Pokedex")),
-      body: BlocBuilder<PokedexBloc, PokedexState>(
-          bloc: BlocProvider.of<PokedexBloc>(context),
-          builder: (context, state) {
-            if (state is PokedexLoadedState)
-              return GridView.count(
-                  crossAxisCount: 2,
-                  children: List.generate(state.list.length, (index) {
-                    return Center(
-                      child: Column(
-                        children: [
-                          Container(
-                            color: ConstsApp.getColorType(
-                                    type: state.list[index].type[0]['type']
-                                        ['name'])
-                                ?.withOpacity(0.7),
-                            child: SvgPicture.network(
-                                state.list[index].sprites["other"]
-                                    ["dream_world"]["front_default"],
-                                height: 100.0,
-                                width: 100.0,
-                                allowDrawingOutsideViewBox: true),
-                          ),
-                          FloatingActionButton(
-                            child: const Icon(Icons.remove),
-                            onPressed: () {
-                              context
-                                  .read<PokedexBloc>()
-                                  .add(PokedexTypesFetchList());
-                            },
-                          ),
-                        ],
-                      ),
-                    );
-                  }));
+      body: Column(
+        children: [
+          BlocBuilder<PokedexCategoriesBloc, PokedexCategoriesState>(
+              bloc: BlocProvider.of<PokedexCategoriesBloc>(context),
+              builder: (context, state) {
+                if (state is PokedexCategoriesLoadedState)
+                  // return Text(state.list[0].name);
+                  return Categories(list: state.list);
 
-            if (state is PokedexTypeLoadedState)
-              return Text(state.list[0].name);
+                return Center(child: CircularProgressIndicator());
+              }),
+          BlocBuilder<PokedexBloc, PokedexState>(
+              bloc: BlocProvider.of<PokedexBloc>(context),
+              builder: (context, state) {
+                if (state is PokedexLoadedState)
+                  // for(var i = 0; i < state.list.length, i++)
+                  return Container(
+                    color: ConstsApp.getColorType(
+                            type: state.list[0].type[0]['type']['name'])
+                        ?.withOpacity(0.7),
+                    child: SvgPicture.network(
+                        state.list[0].sprites["other"]["dream_world"]
+                            ["front_default"],
+                        height: 100.0,
+                        width: 100.0,
+                        allowDrawingOutsideViewBox: true),
+                  );
 
-            return Center(child: CircularProgressIndicator());
-          }),
-      // BlocBuilder<PokedexBloc, PokedexState>(
-      //     bloc: BlocProvider.of<PokedexBloc>(context),
-      //     builder: (context, state) {
-      //       if (state is PokedexTypeLoadedState)
-      //         return Text(state.list[0].name);
+                // return GridView.count(
+                //     crossAxisCount: 2,
+                //     children: List.generate(state.list.length, (index) {
+                //       return Center(
+                //         child: Column(
+                //           children: [
+                //             Container(
+                //               color: ConstsApp.getColorType(
+                //                       type: state.list[index].type[0]['type']
+                //                           ['name'])
+                //                   ?.withOpacity(0.7),
+                //               child: SvgPicture.network(
+                //                   state.list[index].sprites["other"]
+                //                       ["dream_world"]["front_default"],
+                //                   height: 100.0,
+                //                   width: 100.0,
+                //                   allowDrawingOutsideViewBox: true),
+                //             ),
+                //             FloatingActionButton(
+                //               child: const Icon(Icons.remove),
+                //               onPressed: () {
+                //                 context
+                //                     .read<PokedexBloc>()
+                //                     .add(PokedexTypesFetchList());
+                //               },
+                //             ),
+                //           ],
+                //         ),
+                //       );
+                //     }));
 
-      //       return Center(child: CircularProgressIndicator());
-      //     }),
+                return Center(child: CircularProgressIndicator());
+              }),
+        ],
+      ),
     );
   }
 }
---
+++ lib/features/pokedex/presentation/widgets/categories.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:pokedex_flutter/consts/consts_app.dart';
+import 'package:pokedex_flutter/features/pokedex/domain/entities/pokemon_type_entity.dart';
+
+class Categories extends StatelessWidget {
+  final List<PokemonTypeEntity> list;
+  const Categories({Key? key, required this.list}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: [
+          for (var i = 0; i < list.length; i++)
+            Chip(
+              padding: EdgeInsets.all(0),
+              backgroundColor:
+                  ConstsApp.getColorType(type: list[i].name)?.withOpacity(0.8),
+              label: Text(list[i].name, style: TextStyle(color: Colors.white)),
+            ),
+          // Chip(
+          //   padding: EdgeInsets.all(0),
+          //   backgroundColor: Colors.deepPurple,
+          //   label: Text('BADGE', style: TextStyle(color: Colors.white)),
+          // ),
+        ],
+      ),
+    );
+  }
+}

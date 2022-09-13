442e6ea358de665cd0d4368b845dfbd2247879e8
Gabriel <gabrielmd908@gmail.com>
2022-09-12 22:34:12 -0300

    adjust

--- lib/core/error/exception.dart
+++ lib/core/error/exception.dart
@@ -3,9 +3,3 @@ class ServerException implements Exception {
   const ServerException(this.message);
   List<Object> get props => [message];
 }
-
-class CacheException implements Exception {
-  final String message;
-  const CacheException(this.message);
-  List<Object> get props => [message];
-}

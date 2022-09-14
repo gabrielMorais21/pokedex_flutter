1fda6bccaab0c11690217224cb6fa11d3fbc4f15
Gabriel <gabrielmd908@gmail.com>
2022-09-14 20:51:44 -0300

    adjust

--- test/features/data/models/pokemon_model_test.dart
+++ test/features/data/models/pokemon_model_test.dart
@@ -1,5 +1,13 @@
+import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:pokedex_flutter/features/pokedex/data/models/models.dart';
 
 // ignore: must_be_immutable
 class PokemonModelMock extends Mock implements PokemonModel {}
+
+void main() {
+  PokemonModel pokemonModel = PokemonModelMock();
+  test('should return that the PokemonModel is not null', () {
+    expect(pokemonModel, isNotNull);
+  });
+}
--- test/features/data/models/pokemon_type_model_test.dart
+++ test/features/data/models/pokemon_type_model_test.dart
@@ -1,5 +1,13 @@
+import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:pokedex_flutter/features/pokedex/data/models/pokemon_type_model.dart';
 
 // ignore: must_be_immutable
 class PokemonTypeModelMock extends Mock implements PokemonTypeModel {}
+
+void main() {
+  PokemonTypeModel pokemonTypeModel = PokemonTypeModelMock();
+  test('should return that the PokemonTypeModel is not null', () {
+    expect(pokemonTypeModel, isNotNull);
+  });
+}
